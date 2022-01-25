1e71e3a92e6f8654bc70038078ff6c97f843d96e
bemal dvanitha <bdvidanagamachchi@gmail.com>
2022-01-25 07:40:12 +0530

    routes added

---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from './Signup.module.css';
+
+const Signup = () => {
+    return(
+        <div>
+            Signup
+        </div>
+    )
+}
+
+export default Signup;
