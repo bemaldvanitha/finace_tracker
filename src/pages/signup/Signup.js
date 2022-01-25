f77b95f82de3c8ef8a52782f1f58a6b0f7f1d648
bemal dvanitha <bdvidanagamachchi@gmail.com>
2022-01-25 08:16:04 +0530

    signup form added

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from './Signup.module.css';
 
 const Signup = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(email, name, password)
+    }
+
     return(
-        <div>
-            Signup
-        </div>
+        <form onSubmit={submitHandler} className={styles['signup-form']}>
+            <h2>Signup</h2>
+            <label>
+                <span>name:</span>
+                <input type={'text'}
+                       value={name}
+                       onChange={(e) => setName(e.target.value)}/>
+            </label>
+            <label>
+                <span>email:</span>
+                <input type={'email'}
+                       value={email}
+                       onChange={(e) => setEmail(e.target.value)}/>
+            </label>
+            <label>
+                <span>password:</span>
+                <input type={'password'}
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}/>
+            </label>
+            <button className={'btn'}>Signup</button>
+        </form>
     )
 }
 
